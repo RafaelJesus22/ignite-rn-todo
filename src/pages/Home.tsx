ae0334b187dda2fbb6276653837ca871d733a130
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2021-12-01 14:53:32 -0300

    chore: finalizando TaskList e home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,23 +9,38 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          task.done = !task.done;
+        }
+
+        return task;
+      }),
+    );
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const remainingTasks = tasks.filter((task) => task.id !== id);
+    setTasks(remainingTasks);
   }
 
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
-
+      
       <TodoInput addTask={handleAddTask} />
-
+      
       <TasksList 
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
